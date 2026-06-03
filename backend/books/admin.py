62cf8dabb10e7ff6449ff5fa6f37b1fc951a63fa
4e6urek <mamkinhackeryt@gmail.com>
2026-06-03 23:54:23 +0300

    додано адмін панель та виправлено помилку збереження книг

--- backend/books/admin.py
+++ backend/books/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'genre', 'publication_year', 'created_at')
+    search_fields = ('title', 'author')
+    list_filter = ('genre', 'publication_year')
 
 # Register your models here.
