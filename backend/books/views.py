508f596cf29a1fe2591ab2a4239db6a6869a6996
4e6urek <mamkinhackeryt@gmail.com>
2026-06-04 01:25:47 +0300

    feat: додано API ендпоінти для книг та налаштовано права доступу

--- backend/books/views.py
+++ backend/books/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer # <--- Виправляємо тут
+from .permissions import IsAdminOrReadOnly
 
-# Create your views here.
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer # <--- І перевірте, чи правильно тут
+    permission_classes = [IsAdminOrReadOnly]
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer # <--- І тут
+    permission_classes = [IsAdminOrReadOnly]
